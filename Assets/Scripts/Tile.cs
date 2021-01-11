45b51b2e097f4883076463254d12ca85f3a1a283
Shachar <nihohit@gmail.com>
2021-01-11 19:04:29 +0200

    script changes

---
+++ Assets/Scripts/Tile.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tile : MonoBehaviour {
+  float currentMelting = 100f;
+
+  void setMeltingState(float change) {
+    currentMelting -= change;
+  }
+}

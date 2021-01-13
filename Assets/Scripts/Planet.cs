6831c100d49ab4d9f466d3d98d77107a0f895431
Shachar <nihohit@gmail.com>
2021-01-13 22:43:46 +0200

    minor details fixing.

--- Assets/Scripts/Planet.cs
+++ Assets/Scripts/Planet.cs
@@ -5,14 +5,14 @@ using UnityEngine;
 public class Planet : MonoBehaviour {
   private static readonly Quaternion kTopRotation = Quaternion.Euler(90, 0, 0);
   public const float kRadius = 8;
-  public const float kSpaceshipHeight = 3;
+  public const float kSpaceshipHeight = 5;
   public GameObject spaceship;
 
   void Awake() {
     var tilePrefab = Resources.Load<GameObject>("Tile");
     float surfaceArea = 4 * Mathf.PI * Mathf.Pow(kRadius, 2);
-    float tileRadius = tilePrefab.GetComponent<CapsuleCollider>().radius * 3;
-    float tileSurface = Mathf.PI * Mathf.Pow(tileRadius, 2); ;
+    float tileRadius = tilePrefab.GetComponent<CapsuleCollider>().radius;
+    float tileSurface = Mathf.PI * Mathf.Pow(tileRadius, 2);
     int numberOfTiles = Mathf.FloorToInt(surfaceArea / tileSurface);
 
     var Ncount = 0;
@@ -20,7 +20,7 @@ public class Planet : MonoBehaviour {
     var d = Mathf.Sqrt(a);
     var M_theta = Mathf.Round(Mathf.PI / d);
     var d_theta = Mathf.PI / M_theta;
-    var d_phi = a / (d_theta);
+    var d_phi = a / d_theta;
     for (var m = 0; m < M_theta; ++m) {
       var _theta = Mathf.PI * (m + 0.5f) / M_theta;
       var M_phi = Mathf.Round(2 * Mathf.PI * Mathf.Sin(_theta) / d_phi);
@@ -37,14 +37,6 @@ public class Planet : MonoBehaviour {
 
     spaceship.transform.position = new Vector3(0, kSpaceshipHeight + kRadius, 0);
   }
-
-  //     for (int i = 0; i<numberOfTiles; ++i) {
-  //       Vector3 position = Vector3.zero;
-  //   Quaternion rotation = Quaternion.identity;
-  //   var tile = Instantiate(tilePrefab, position, rotation);
-  //   tile.transform.parent = transform;
-  //     }
-  //   }
 
   // Update is called once per frame
   void Update() { }
